3b7f7e315a61b8756130906a77e88c2eea3eb4a6
szoszo23 <szolnoki96@gmail.com>
2024-04-04 13:43:40 +0200

    SolarController.cs created.

---
+++ Solar_Watch/Solar_Watch/Controllers/SolarController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Solar_Watch.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class SolarController : ControllerBase
+{
+    private readonly ILogger<SolarController> _logger;
+
+    public SolarController(ILogger<SolarController> logger)
+    {
+        _logger = logger;
+    }
+}
