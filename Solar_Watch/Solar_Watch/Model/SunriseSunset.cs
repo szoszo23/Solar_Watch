d22a469392aa6356a7368a2c08ec9312deef3d3c
szoszo23 <szolnoki96@gmail.com>
2024-04-04 13:29:51 +0200

    SunriseSunset.cs created

---
+++ Solar_Watch/Solar_Watch/Model/SunriseSunset.cs
@@ -0,0 +1,10 @@
+﻿namespace Solar_Watch.Model;
+
+public class SunriseSunset
+{
+    public int SunriseSunsetId { get; set; }
+    public City City { get; set; }
+    public DateTime Sunrise { get; set; }
+    public DateTime Sunset { get; set; }
+    
+}
