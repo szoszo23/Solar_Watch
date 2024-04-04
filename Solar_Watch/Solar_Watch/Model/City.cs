4e79c01b12d49d3f488dbc0bb3d4a295045e8358
szoszo23 <szolnoki96@gmail.com>
2024-04-04 13:28:15 +0200

    City class created.

---
+++ Solar_Watch/Solar_Watch/Model/City.cs
@@ -0,0 +1,11 @@
+﻿namespace Solar_Watch.Model;
+
+public class City
+{
+    public int CityId { get; }
+    public string Name { get; set; }
+    public GeoCoordinates Coordinates { get; set; }
+    public string Country { get; set; }
+    public SunriseSunset SunriseSunset { get; set; }
+    public int SunriseSunsetId { get; set; }
+}
