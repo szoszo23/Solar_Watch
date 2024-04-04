1024e8b4ab19e14261fc2de8ee918d294a81f3ac
szoszo23 <szolnoki96@gmail.com>
2024-04-04 13:30:02 +0200

    SolarWatch.cs created

---
+++ Solar_Watch/Solar_Watch/Model/SolarWatch.cs
@@ -0,0 +1,10 @@
+﻿namespace Solar_Watch.Model;
+
+public class SolarWatch
+{
+    public DateTime Date { get; set; }
+    public string City { get; set; }
+    public DateTime Sunrise { get; set; }
+    public DateTime Sunset { get; set; }
+
+}
