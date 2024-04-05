c3159621a3b4fc5529653f98ffca0d5d58a50b91
szoszo23 <szolnoki96@gmail.com>
2024-04-05 14:29:18 +0200

    IGeoCodingApi interface and GeoCodingApi created

---
+++ Solar_Watch/Solar_Watch/Services/Utilities/GeoCodingApi.cs
@@ -0,0 +1,30 @@
+﻿using System.Net;
+
+namespace Solar_Watch.Services.Utilities;
+
+public class GeoCodingApi : IGeoCodingApi
+{
+    private readonly ILogger<GeoCodingApi> _logger;
+    private readonly string _apiKey;
+    
+    public GeoCodingApi(ILogger<GeoCodingApi> logger, string apiKey)
+    {
+        _logger = logger;
+        _apiKey = apiKey;
+    }
+    
+    public async Task<string> GetCoordinates(string city)
+    {
+        var url = $"http://api.openweathermap.org/geo/1.0/direct?q={city}&limit={1}&appid={_apiKey}";
+
+        using var client = new HttpClient();
+        var response = await client.GetAsync(url);
+        _logger.LogInformation($"Received data from API: {response}");
+        if( response.StatusCode == HttpStatusCode.NotFound)
+        {
+            return null;
+        }
+
+        return await response.Content.ReadAsStringAsync();
+    }
+}
---
+++ Solar_Watch/Solar_Watch/Services/Utilities/IGeoCodingApi.cs
@@ -0,0 +1,6 @@
+﻿namespace Solar_Watch.Services.Utilities;
+
+public interface IGeoCodingApi
+{
+    Task<string> GetCoordinates(string city);
+}
