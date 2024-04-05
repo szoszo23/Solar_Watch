ecf40c1ac8a16c1579725adc8456311552b1c510
szoszo23 <szolnoki96@gmail.com>
2024-04-05 18:36:13 +0200

    ISunApi.cs GetSunriseSunset method added

--- Solar_Watch/Solar_Watch/Services/Utilities/ISunApi.cs
+++ Solar_Watch/Solar_Watch/Services/Utilities/ISunApi.cs
@@ -1,6 +1,8 @@
-﻿namespace Solar_Watch.Services.Utilities;
+﻿using Solar_Watch.Model;
+
+namespace Solar_Watch.Services.Utilities;
 
 public interface ISunApi
 {
-    
+    Task<string> GetSunriseSunset(GeoCoordinates coordinates, DateTime date);
 }
