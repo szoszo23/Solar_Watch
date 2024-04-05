17c481303f1488519d0cd6c011c1c126bd28e39d
szoszo23 <szolnoki96@gmail.com>
2024-04-05 16:37:36 +0200

    IJsonProcessor interface created

---
+++ Solar_Watch/Solar_Watch/Services/Utilities/IJsonProcessor.cs
@@ -0,0 +1,11 @@
+﻿using Solar_Watch.Model;
+
+namespace Solar_Watch.Services.Utilities;
+
+public interface IJsonProcessor
+{
+    Task<GeoCoordinates> ProcessCoordinatesJson(Task<string> data);
+
+    Task<SunriseSunset> ProcessSunriseSunsetJson(Task<string> dataTask);
+    string ProcessCityJson(string data);
+}
