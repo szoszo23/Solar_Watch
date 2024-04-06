ef07c87bb062e3b83c9ff5b2042517b3d2e6c8d3
szoszo23 <szolnoki96@gmail.com>
2024-04-06 16:44:25 +0200

    SolarWatchDbContext.cs created and OnModelCreating implemented

---
+++ Solar_Watch/Solar_Watch/Services/SolarWatchDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Solar_Watch.Model;
+
+namespace Solar_Watch.Services;
+
+public class SolarWatchDbContext : DbContext
+{
+    public DbSet<City> Cities { get; set; }
+    public DbSet<SunriseSunset> SunriseSunsets { get; set; }
+
+    public SolarWatchDbContext(DbContextOptions<SolarWatchDbContext> options) : base(options)
+    {
+        
+    }
+    
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        builder.Entity<City>().HasKey(c => c.CityId);
+        builder.Entity<City>().OwnsOne(c => c.Coordinates);
+        builder.Entity<City>().HasIndex(c => c.Name).IsUnique();
+        
+        builder.Entity<SunriseSunset>()
+            .HasOne(ss => ss.City)
+            .WithOne(c => c.SunriseSunset)
+            .HasForeignKey<City>(c => c.SunriseSunsetId);
+    }
+}
