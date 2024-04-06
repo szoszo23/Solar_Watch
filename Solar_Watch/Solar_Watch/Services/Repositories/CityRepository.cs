a7064466754e66cc44950f0a0b8d66f3ee2f5cc1
szoszo23 <szolnoki96@gmail.com>
2024-04-06 16:46:03 +0200

    CityRepository.cs repository pattern created and CRUD operations implemented

---
+++ Solar_Watch/Solar_Watch/Services/Repositories/CityRepository.cs
@@ -0,0 +1,46 @@
+﻿using Solar_Watch.Model;
+
+namespace Solar_Watch.Services.Repositories;
+
+public class CityRepository
+{
+    private SolarWatchDbContext _dbContext;
+
+    public CityRepository(SolarWatchDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+    
+    public IEnumerable<City> GetAll()
+    {
+
+        return _dbContext.Cities.ToList();
+    }
+
+    public City? GetByName(string name)
+    {
+
+        return _dbContext.Cities.FirstOrDefault(c => c.Name == name);
+    }
+
+    public void Add(City city)
+    {
+
+        _dbContext.Add(city);
+        _dbContext.SaveChanges();
+    }
+
+    public void Delete(City city)
+    {
+
+        _dbContext.Remove(city);
+        _dbContext.SaveChanges();
+    }
+
+    public void Update(City city)
+    {
+
+        _dbContext.Update(city);
+        _dbContext.SaveChanges();
+    }
+}
