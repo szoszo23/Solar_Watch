ddd4561059c593967bd4778bb5e5cade26d7a31b
szoszo23 <szolnoki96@gmail.com>
2024-04-08 01:29:58 +0200

    UserContext.cs created

---
+++ Solar_Watch/Solar_Watch/Services/UserContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Solar_Watch.Services;
+
+public class UserContext : IdentityDbContext<IdentityUser, IdentityRole, string>
+{
+    public UserContext (DbContextOptions<UserContext> options)
+        : base(options)
+    {
+    }
+    
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+    }
+}
