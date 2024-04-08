59c62557deaf4b495d064e7411372458f4fda27e
szoszo23 <szolnoki96@gmail.com>
2024-04-08 11:06:50 +0200

    AuthController.cs created

---
+++ Solar_Watch/Solar_Watch/Controllers/AuthController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Solar_Watch.Services.Authentication;
+
+namespace Solar_Watch.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class AuthController : ControllerBase
+{
+    private readonly IAuthService _authenticationService;
+
+    public AuthController(IAuthService authenticationService)
+    {
+        _authenticationService = authenticationService;
+    }
+}
