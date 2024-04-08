cca4e35e1fba41a36e61af4f0d81add6172a78b3
szoszo23 <szolnoki96@gmail.com>
2024-04-08 13:28:25 +0200

    CreateAdminRole method implemented

--- Solar_Watch/Solar_Watch/Services/Authentication/AuthenticationSeeder.cs
+++ Solar_Watch/Solar_Watch/Services/Authentication/AuthenticationSeeder.cs
@@ -12,4 +12,10 @@ public class AuthenticationSeeder
         this.roleManager = roleManager;
         this.userManager = userManager;
     }
+    
+    
+    private async Task CreateAdminRole(RoleManager<IdentityRole> roleManager)
+    {
+        await roleManager.CreateAsync(new IdentityRole("Admin")); //The role string should better be stored as a constant or a value in appsettings
+    }
 }
