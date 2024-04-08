1071093eae3ec42ddd7e37791e4aa2426fd41369
szoszo23 <szolnoki96@gmail.com>
2024-04-08 13:27:11 +0200

    AuthenticationSeeder.cs created

---
+++ Solar_Watch/Solar_Watch/Services/Authentication/AuthenticationSeeder.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Solar_Watch.Services.Authentication;
+
+public class AuthenticationSeeder
+{
+    private RoleManager<IdentityRole> roleManager;
+    private UserManager<IdentityUser> userManager;
+
+    public AuthenticationSeeder(RoleManager<IdentityRole> roleManager, UserManager<IdentityUser> userManager)
+    {
+        this.roleManager = roleManager;
+        this.userManager = userManager;
+    }
+}
