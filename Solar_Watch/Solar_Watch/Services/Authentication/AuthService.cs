f29296cade9502020bc3b4a2c99d078e44f4ee4e
szoszo23 <szolnoki96@gmail.com>
2024-04-08 10:58:14 +0200

    AuthService.cs created

---
+++ Solar_Watch/Solar_Watch/Services/Authentication/AuthService.cs
@@ -0,0 +1,10 @@
+﻿namespace Solar_Watch.Services.Authentication;
+
+public class AuthService : IAuthService
+{
+
+    public async Task<AuthResult> RegisterAsync(string email, string username, string password)
+    {
+        return new AuthResult(true, email, username, "");
+    }
+}
