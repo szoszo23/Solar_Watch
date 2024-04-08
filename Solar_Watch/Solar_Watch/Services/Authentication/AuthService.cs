f9ee6feed4523786d7100f0ddf27aa6e5bcb8a3d
szoszo23 <szolnoki96@gmail.com>
2024-04-08 10:59:36 +0200

    AuthService constructor implemented

--- Solar_Watch/Solar_Watch/Services/Authentication/AuthService.cs
+++ Solar_Watch/Solar_Watch/Services/Authentication/AuthService.cs
@@ -1,7 +1,15 @@
-﻿namespace Solar_Watch.Services.Authentication;
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Solar_Watch.Services.Authentication;
 
 public class AuthService : IAuthService
 {
+    private readonly UserManager<IdentityUser> _userManager;
+
+    public AuthService(UserManager<IdentityUser> userManager)
+    {
+        _userManager = userManager;
+    }
 
     public async Task<AuthResult> RegisterAsync(string email, string username, string password)
     {
