531e8bef0439bb7409f85a2aa0727b74ab8380e5
szoszo23 <szolnoki96@gmail.com>
2024-04-08 11:44:14 +0200

    CreateSigningCredentials helper method implemented

--- Solar_Watch/Solar_Watch/Services/Authentication/TokenService.cs
+++ Solar_Watch/Solar_Watch/Services/Authentication/TokenService.cs
@@ -1,14 +1,31 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using System.Text;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.IdentityModel.Tokens;
 
 namespace Solar_Watch.Services.Authentication;
 
 public class TokenService : ITokenService
 {
+    private readonly IConfiguration _configuration;
     private const int ExpirationMinutes = 30;
 
+    public TokenService(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
     public string CreateToken(IdentityUser user)
     {
 
         return null;
+    }
+    
+    private SigningCredentials CreateSigningCredentials()
+    {
+        return new SigningCredentials(
+            new SymmetricSecurityKey(
+                Encoding.UTF8.GetBytes(_configuration.GetSection("IssuerSigningKey").Value)
+            ),
+            SecurityAlgorithms.HmacSha256
+        );
     }
 }
