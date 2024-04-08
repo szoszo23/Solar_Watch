c70c0e844a4a44b84fbe5374c1d305864e349f85
szoszo23 <szolnoki96@gmail.com>
2024-04-08 11:35:06 +0200

    TokenService.cs class created.

---
+++ Solar_Watch/Solar_Watch/Services/Authentication/TokenService.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Solar_Watch.Services.Authentication;
+
+public class TokenService : ITokenService
+{
+    private const int ExpirationMinutes = 30;
+
+    public string CreateToken(IdentityUser user)
+    {
+
+        return null;
+    }
+}
