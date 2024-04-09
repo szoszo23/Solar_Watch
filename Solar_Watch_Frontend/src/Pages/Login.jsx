62ff24f7f1f48a298ca4e6a911b4d4c0613a7659
szoszo23 <szolnoki96@gmail.com>
2024-04-09 13:37:48 +0200

    Login form implemented

--- Solar_Watch_Frontend/src/Pages/Login.jsx
+++ Solar_Watch_Frontend/src/Pages/Login.jsx
@@ -2,8 +2,25 @@
 import { useNavigate } from "react-router-dom";
 
 function Login() {
-    
-    return(<div></div>)
+    const navigate = useNavigate();
+    async function handleSubmit(event) {
+        
+    }
+    return(<div>
+        <h2>Login</h2>
+        <form className="card" onSubmit={handleSubmit}>
+            <label>Email:</label>
+            <br/>
+            <input type="email" name="email"/>
+            <br/>
+            <label>Password:</label>
+            <br/>
+            <input type="password" name="password"/>
+            <br/>
+            <br/>
+            <button type="submit">Login</button>
+        </form>
+    </div>)
 }
 
 export default Login;
