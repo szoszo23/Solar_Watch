8b40573f97d44f77da8fe070deef51f32a4f8d7b
szoszo23 <szolnoki96@gmail.com>
2024-04-09 11:21:51 +0200

    React routing implemented

--- Solar_Watch_Frontend/src/main.jsx
+++ Solar_Watch_Frontend/src/main.jsx
@@ -1,10 +1,36 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from './App.jsx'
+import Register from "./Pages/Register.jsx";
+import Login from "./Pages/Login.jsx";
+import SolarWatch from "./Pages/SolarWatch.jsx"
 import './index.css'
 
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <App/>,
+        children:[
+            {
+                path: "/register",
+                element: <Register/>
+            },
+            {
+                path: "/login",
+                element: <Login/>
+            },
+            {
+                path: "/solar-watch",
+                element: <SolarWatch/>
+            }
+        ]
+    }
+])
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <React.StrictMode>
+        <RouterProvider router={router}/>
+    </React.StrictMode>,
 )
