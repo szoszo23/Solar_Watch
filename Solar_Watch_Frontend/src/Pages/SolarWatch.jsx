69e80ac9c762b3f949d6978948853fc70fb01565
szoszo23 <szolnoki96@gmail.com>
2024-04-09 13:52:52 +0200

    Creating variables and useStates. Creating Solar Watch form. clearForm function implemented.

--- Solar_Watch_Frontend/src/Pages/SolarWatch.jsx
+++ Solar_Watch_Frontend/src/Pages/SolarWatch.jsx
@@ -1,9 +1,76 @@
-﻿import React from "react";
+﻿import React, {useState} from "react";
 import { useNavigate } from "react-router-dom";
 
 function SolarWatch() {
+    const navigate = useNavigate();
+    const currentDate = new Date();
+    const [sunriseTime, setSunriseTime] = useState("");
+    const [sunsetTime, setSunsetTime] = useState("");
+    const [date, setDate] = useState("");
+    const [city, setCity] = useState("");
+    const [hideResult, setHideResult] = useState(true);
+    const [loading, setLoading] = useState(true);
 
-    return(<div></div>)
+    
+    
+    function clearForm() {
+        setCity("");
+        setDate(formattedDate);
+        setSunriseTime("");
+        setSunsetTime("");
+        setHideResult(true);
+        // clear the form inputs
+        document.getElementsByName("city")[0].value = "";
+        document.getElementsByName("date")[0].value = formattedDate;
+    }
+    return(<div>
+            <h2>SolarWatch</h2>
+            <div className="card" hidden={hideResult === true ? false : true}>
+                <form onSubmit={handleSubmit}>
+                    <label>City name:</label>
+                    <br/>
+                    <input
+                        required={true}
+                        type="text"
+                        name="city"
+                        onChange={(e) => setCity(e.target.value)}
+                        autoComplete="off"
+                    />
+                    <br/>
+                    <label>Date:</label>
+                    <br/>
+                    <input
+                        type="date"
+                        name="date"
+                        value={date}
+                        onChange={(e) => setDate(e.target.value)}
+                    />
+                    <br/>
+                    <br/>
+                    <button type="submit">Search</button>
+                </form>
+            </div>
+            <div hidden={loading}>
+                <h3>Loading...</h3>
+            </div>
+            <div className="card" hidden={hideResult}>
+                <h3>Sunrise and Sunset times in:</h3>
+                <h3>{city}</h3>
+                <h3>at:</h3>
+                <h3>{date}</h3>
+                <h3>Sunrise time (UTC):</h3>
+                <h3>{sunriseTime}</h3>
+                <h3>Sunset time (UTC):</h3>
+                <h3>{sunsetTime}</h3>
+                <button type="button" onClick={() => clearForm()}>
+                    Clear
+                </button>
+            </div>
+            <button type="button" onClick={() => navigate("/")}>
+                Cancel
+            </button>
+        </div>
+    )
 }
 
 export default SolarWatch;
