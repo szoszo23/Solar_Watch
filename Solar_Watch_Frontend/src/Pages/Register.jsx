e4a6b49e0f897a5bb21b17e705acc3657695c4e1
szoszo23 <szolnoki96@gmail.com>
2024-04-09 13:25:57 +0200

    register form implemented

--- Solar_Watch_Frontend/src/Pages/Register.jsx
+++ Solar_Watch_Frontend/src/Pages/Register.jsx
@@ -2,7 +2,27 @@
 
 function Register() {
 
-    return(<div></div>)
+    
+    function handleSubmit(event){
+        
+    }
+    return(<div className="registration">
+        <form className="registrationForm" onSubmit={handleSubmit}>
+            <label>Username:</label>
+            <br></br>
+            <input type="text" name="username"></input>
+            <br></br>
+            <label>Email:</label>
+            <br></br>
+            <input type="email" name="email"></input>
+            <br></br>
+            <label>Password:</label>
+            <br></br>
+            <input type="password" name="password"></input>
+            <br></br>
+            <button type="submit">Register</button>
+        </form>
+    </div>)
 }
 
 export default Register;
