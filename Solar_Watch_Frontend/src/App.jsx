7ad5db199477bd2444e32b1f8f8fad6e5e447ae7
szoszo23 <szolnoki96@gmail.com>
2024-04-10 13:56:24 +0200

    Update App.jsx
    Fetch WhoAmI to store username and email

--- Solar_Watch_Frontend/src/App.jsx
+++ Solar_Watch_Frontend/src/App.jsx
@@ -1,7 +1,40 @@
-import { Outlet, Link } from "react-router-dom";
+import { Outlet, Link, useLocation} from "react-router-dom";
+import { useEffect, useState } from "react";
 import './App.css'
 
 function App() {
+    const [user, setUser] = useState(null);
+    const [email, setEmail] = useState(null);
+    const location = useLocation();
+
+    useEffect(() => {
+        async function fetchData() {
+            try {
+                const response = await fetch("/api/Auth/WhoAmI", {
+                    method: "GET",
+                    credentials: "include",
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                });
+                
+                if(!response.ok) {
+                    throw new Error("Network response was not ok")
+                }
+                const data = await response.json();
+                console.log(data);
+                if (data) {
+                    setUser(data.userName);
+                    setEmail(data.email);
+                }
+            } catch (error) {
+                console.log("Error", error);
+            }
+        }
+        fetchData();
+    }, [location.pathname]);
+    
+    
     return (
         <div>
             <header className="App-header">
