e49d1b58e5a2baa9e59b0557914d2699b00582a5
szoszo23 <szolnoki96@gmail.com>
2024-04-10 13:30:20 +0200

    Token parameter deleted from IAuthResponse

--- Solar_Watch/Solar_Watch/Services/Authentication/AuthResponse.cs
+++ Solar_Watch/Solar_Watch/Services/Authentication/AuthResponse.cs
@@ -1,3 +1,3 @@
 ﻿namespace Solar_Watch.Services.Authentication;
 
-public record AuthResponse(string Email, string UserName, string Token);
+public record AuthResponse(string Email, string UserName);
--- Solar_Watch/Solar_Watch/Services/Authentication/IAuthService.cs
+++ Solar_Watch/Solar_Watch/Services/Authentication/IAuthService.cs
@@ -1,7 +1,10 @@
-﻿namespace Solar_Watch.Services.Authentication;
+﻿using System.IdentityModel.Tokens.Jwt;
+
+namespace Solar_Watch.Services.Authentication;
 
 public interface IAuthService
 {
     Task<AuthResult> RegisterAsync(string email, string username, string password, string role);
     Task<AuthResult> LoginAsync(string email, string password);
+    JwtSecurityToken Verify(string token);
 }
